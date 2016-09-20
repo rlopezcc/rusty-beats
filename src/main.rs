782eacc16e87dbf082ef2f400c4ff8827b023f7c
rlopezcc <rlopez@sophilabs.com>
2016-09-19 23:59:01 -0300

    Add basic trackfile functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,62 @@
 extern crate ears;
 use ears::{Sound, AudioController};
 use std::collections::HashMap;
+use std::path::Path;
+use std::fs::File;
+use std::error::Error;
+use std::io::Read;
+use std::{thread, time};
+
+
+#[derive(Debug)]
+struct Track {
+    name: String,
+    data: Vec<i32>
+}
+
+impl Track {
+    pub fn from_trackfile_line(line: &str) -> Track{
+        let mut splitted = line.split_whitespace();
+        let name: String = splitted.next().unwrap().to_string();
+        let mut data: Vec<i32> = Vec::new();
+        for _ in splitted {
+            data.push(1);
+            data.push(0);
+        }
+        return Track {name: name, data: data};
+    }
+}
 
 fn main () {
+    let mut trackfile = match File::open(&Path::new("tracks.txt")) {
+        Err(why) => panic!("couldn't open tracks.txt: {}", why.description()),
+        Ok(file) => file
+    };
+
+    let mut contents: String = String::new(); 
+    match trackfile.read_to_string(&mut contents) {
+        Err(_) => panic!("Cannot read trackfile"),
+        Ok(string) => string
+    };
+
+    let track: Track = Track::from_trackfile_line(&contents.lines().next().unwrap());
+
     let mut sounds = HashMap::new();
 
-    let name:&str = "sample1";
-    let path:String = "src/sounds/".to_string() + name + ".ogg";
+    let path: String = "sounds/".to_string() + &track.name + ".ogg";
 
-    // Create a new Sound.
-    sounds.insert(name, Sound::new(&path).unwrap());
+    sounds.insert(&track.name, Sound::new(&path).unwrap());
 
-    // Play the Sound
-    match sounds.get_mut(name) {
-        Some(mut sound) => {sound.play(); while sound.is_playing() {}},
-        _ => println!("couldn't play {}", name)
+    let ten_millis = time::Duration::from_millis(500);
+
+    for i in 0..track.data.len() {
+        if track.data[i] != 0 {
+            match sounds.get_mut(&track.name) {
+                Some(mut sound) => {sound.play()},
+                _ => println!("couldn't play {}", &track.name)
+            }
+        }
+        thread::sleep(ten_millis);
     }
+
 }
